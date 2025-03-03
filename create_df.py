f9b902eb99ad698da1d4aea135a8b8964b814fe4
ann-e888 <132256907+ann-e888@users.noreply.github.com>
2025-03-03 17:13:51 +0100

    New files.

--- create_df.py
+++ create_df.py
@@ -1,17 +1,11 @@
 import os               
-import ast
 import re
 import pdfplumber
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-from IPython.display import display
 from pandasgui import show
-
-sns.set_theme()
 
 pdfs_short = 'pdfs/short'
 pdf_free = 'pdfs/free'
