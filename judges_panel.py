26c68695caad7173bffeee1359c0b3e66c0bc496
ann-e888 <132256907+ann-e888@users.noreply.github.com>
2025-03-26 17:28:03 +0100

    Fix on Japan in judges panels.

--- judges_panel.py
+++ judges_panel.py
@@ -17,8 +17,14 @@ competitions = ['fc2025', 'fc2024', 'fc2023', 'fc2022', 'fc2021',
                 'gpf2024', 'gpf2023', 'gpf2022', 'gpf2021',
                 'gprus2021', 'gpita2021']
 segments = ['SEG001', 'SEG002', 'SEG003', 'SEG004']
+segments_japan = ['data0130', 'data0150', 'data0230', 'data0250']
 
-print(len(seasons)*len(competitions)*len(segments))
+def process_url(season, com, seg, seg_jpn):
+  if com in ['wc2023', 'gpjpn2024', 'gpjpn2023', 'gpjpn2022', 'gpjpn2021']:
+      url = f'https://results.isu.org/results/{season}/{com}/{seg_jpn}.htm'
+  else:
+      url = f'https://results.isu.org/results/{season}/{com}/{seg}OF.htm'
+  return url
 
 def get_judges_panel(url):
 
@@ -33,9 +39,10 @@ def get_judges_panel(url):
   if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html')
 
-    judge_rows = soup.find_all('tr', class_=['Line1Blue', 'Line2Blue', 
-                                             'Line1Red', 'Line2Red'
-                                             'BlueL', 'RedL'])
+    judge_rows = soup.find_all('tr', class_= ['Line1Blue', 'Line2Blue',
+                                              'Line1Red', 'Line2Red',
+                                              'Blue0', 'Blue1',
+                                              'Red0', 'Red1'])
 
     for row in judge_rows:
       cells = row.find_all('td')
@@ -49,19 +56,29 @@ def get_judges_panel(url):
   return judges
 
 urls = []
+judges_panels_list = []
 count = 0
 
 for season in seasons:
   for com in competitions:
-    for seg in segments:
-      url = f'https://results.isu.org/results/{season}/{com}/{seg}OF.htm'
-      urls.append(url)
+    if com in ['wc2023', 'gpjpn2024', 'gpjpn2023', 'gpjpn2022', 'gpjpn2021']:
+      for seg_jpn in segments_japan:  
+        urls.append(process_url(season, com, None, seg_jpn))
+    else:
+      for seg in segments:  
+          urls.append(process_url(season, com, seg, None))
+
+for url in urls:
+  judges = get_judges_panel(url)
+  print(judges)
 
 for url in urls:
   judges = get_judges_panel(url)
   print(judges)
 
   if judges:
+    judges_panels_list.append(judges)
     count += 1
 
 print(count)
+print(judges_panels_list)
